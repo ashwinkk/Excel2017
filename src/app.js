8cc0ed0a3e517617cc7d3bac85ca65dd99337a9a
ashwinkk <ashwinkailas@gmail.com>
2017-08-10 15:34:25 +0530

    navbar updated

--- src/app.js
+++ src/app.js
@@ -4,18 +4,24 @@ import { Route, Switch } from "react-router-dom";
 import Competitions from "./pages/competitions";
 import CompetitionDetail from "./pages/competition-detail";
 import Gallery from "./pages/gallery.js";
+import Navbar from "./partials/navbar";
 
-import './styles/defaults.css';
+import "./styles/defaults.css";
 
 function App(props) {
-  return (
-    <Switch>
-      <Route exact path="/" render={() => <h1>Hello world</h1>} />
-      <Route path="/events/:id" component={CompetitionDetail} />
-      <Route path="/events" component={Competitions} />
-      <Route path="/gallery" component={Gallery} />
-    </Switch>
-  );
+	return (
+		<div>
+			<Navbar />
+			<div className="content">
+				<Switch>
+					<Route exact path="/" render={() => <h1>Hello world</h1>} />
+					<Route path="/events/:id" component={CompetitionDetail} />
+					<Route path="/events" component={Competitions} />
+					<Route path="/gallery" component={Gallery} />
+				</Switch>
+			</div>
+		</div>
+	);
 }
 
 export default App;
