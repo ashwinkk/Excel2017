4b99705c7e519d043ab47eafad99e62eee6cb5d0
Navin Mohan <navinmohan81@gmail.com>
2017-08-15 23:22:27 +0530

    init

--- src/app.js
+++ src/app.js
@@ -1,21 +1,46 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import Competitions from "./pages/competitions";
 import CompetitionDetail from "./pages/competition-detail";
-import Gallery from "./pages/gallery.js";
+import WorkshopDetail from "./pages/workshop-detail";
+// import Gallery from "./pages/gallery.js";
+import Navbar from "./partials/navbar";
+import ComingSoon from "./pages/coming-soon";
+import Spotlight from "./pages/spotlight";
+import Events from "./pages/events";
+import CountDown from "./partials/countdown-ringer";
 
-import './styles/defaults.css';
+import "./styles/defaults.css";
+import "./styles/test.scss";
 
 function App(props) {
-  return (
-    <Switch>
-      <Route exact path="/" render={() => <h1>Hello world</h1>} />
-      <Route path="/events/:id" component={CompetitionDetail} />
-      <Route path="/events" component={Competitions} />
-      <Route path="/gallery" component={Gallery} />
-    </Switch>
-  );
+	const options = {
+		textColor: "#000",
+		endDate: "2017-09-09",
+		topCircleColor: "rgba(33,15,255,0.5)",
+		bottomCircleColor: "rgba(90,77,245,1)" 
+	};
+	return (
+		<div>
+			<Navbar />
+			<div className="content">
+				<Switch>
+					<Route
+						exact
+						path="/"
+						render={() => <Redirect to="/under-construction" />}
+					/>
+					<Route path="/countdown" render={() => <h1>Hello world<br/><CountDown options={{ endDate: new Date("2017-08-20") }}/></h1>} />
+					<Route path="/events/:type" component={WorkshopDetail} />
+					<Route path="/spotlight" component={Spotlight} />
+					<Route path="/events" component={Events} />
+					<Route path="/competitions" component={Competitions} />
+					<Route path="/under-construction" component={ComingSoon} />
+				</Switch>
+			</div>
+		</div>
+	);
 }
 
 export default App;
