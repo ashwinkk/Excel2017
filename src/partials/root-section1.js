01d69a7a34292032edf0a9860597d5defba94fee
ashwinkk <ashwinkailas@gmail.com>
2017-09-11 09:52:01 +0530

    parallax initial

--- src/partials/root-section1.js
+++ src/partials/root-section1.js
@@ -1,14 +1,41 @@
 import React from "react";
+import { ParallaxController, Parallax } from "react-scroll-parallax";
 
-import "../styles/root-section2.css";
+import "../styles/root-section1.css";
+
+ParallaxController.init();
 
 export default class RootSection1 extends React.Component {
 	render() {
+		let headerLayer = (
+			<Parallax offsetYMax={"0px"} offsetYMin={"-500px"} slowerScrollRate>
+				<div className="header-layer">
+					<img src="/static/anim-assets/header.png" alt="cloud-top" />
+				</div>
+			</Parallax>
+		);
+		let footerLayer = (
+			<Parallax
+				offsetYMax={100}
+				offsetYMin={-100}
+				slowerScrollRate
+				tag="figure"
+			>
+				<div className="footer-layer">
+					<img src="/static/anim-assets/footer.png" alt="cloud-bottom" />
+				</div>
+			</Parallax>
+		);
 		return (
-			<div
-				className="root-section"
-				style={{ background: "rgba(255,0,0,0.2)", zIndex: 90 }}
-			/>
+			<div style={{ zIndex: 90, position: "relative" }}>
+				<Parallax offsetYMax={30} offsetYMin={-200} slowerScrollRate>
+					<img
+						className="header-cloud"
+						src="/static/anim-assets/header.png"
+						alt="cloud-top"
+					/>
+				</Parallax>
+			</div>
 		);
 	}
 }
