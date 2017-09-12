9edbcf4ac4dae65d2f8c8c55d15ef14f9021cfb4
Navin Mohan <navinmohan81@gmail.com>
2017-09-12 11:36:30 +0530

    speech recognition

--- src/partials/bot-area.js
+++ src/partials/bot-area.js
@@ -1,21 +1,46 @@
 import React from "react";
+import SpeechRecognition from "react-speech-recognition";
+import FaMicrophone from "react-icons/lib/fa";
+
 
 import "../styles/bot-area.css";
 
+
+
+const Dots = () => (
+		<div id="wave">
+			<span className="dot" />
+			<span className="dot" />
+			<span className="dot" />
+		</div>
+);
+
+const ListenButton = (props) => (
+	<div className="listen-btn" onClick={props.listenOnClick}>
+		<FaMicrophone/>
+	</div>
+);
+
+
+
+@SpeechRecognition({
+	autoStart: false
+})
 class BotChat extends React.Component {
+
 	render() {
 		let className = "container bot-chat-container";
 		if (this.props.spawn) className += " spawn";
+
+		console.log(this.props.startListening);
+		const status = this.props.listening?<Dots/>:<ListenButton listenOnClick={this.props.startListening}/>;
+
 		return (
 			<div className={className}>
 				<div className="bot-chat">
 					<h3 className="text-center">How may I help you?</h3>
-					<div id="wave">
-						<span className="dot" />
-						<span className="dot" />
-						<span className="dot" />
-					</div>
-					<h3 className="text-center">COMING SOON</h3>
+					{status}
+					<p>{this.props.transcript}</p>
 				</div>
 			</div>
 		);
