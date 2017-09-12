56dafa84cbcd8eed82f15bd76e0fe4a55b9da13d
Navin Mohan <navinmohan81@gmail.com>
2017-09-12 19:42:43 +0530

    added gallery slider

---
+++ src/pages/gallery1.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchImages } from "../actions/gallery-actions";
+
+
+
+import GalleryImageGrid from "../partials/gallery-img-grid";
+import GalleryCarousal from "../partials/gallery-carousal";
+
+
+import "../styles/gallery1.css";
+
+@connect(store => {
+	return {
+		images: store.gallery.images,
+		fetched: store.gallery.fetched,
+		fetching: store.gallery.fetching
+	};
+})
+export default class Gallery extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            selectedImage: 0,
+        };
+    }
+    
+    componentWillMount() {
+		this.props.dispatch(fetchImages());
+    }
+
+    changeSelectedImage(i){
+        this.setState({
+            ...this.state,
+            selectedImage: i
+        });
+    }
+    
+
+    render(){
+        console.log("selected img:",this.state.selectedImage);
+        return(
+            <div className="row gallery-container" >
+                <div className="col-xs-12">
+                    <h1 className="gallery-heading">Gallery</h1>
+                </div>
+                <div className="col-xs-12 col-md-8" >
+                    <GalleryCarousal images={this.props.images} selectedImage={this.state.selectedImage} />
+                </div>
+                <div className="col-xs-12 col-md-4">
+                    <GalleryImageGrid
+                        images={this.props.images}
+                        onSelectImage={this.changeSelectedImage.bind(this)}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
