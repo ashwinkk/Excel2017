05c7e3aafc0f6ae0ee4cf1db2c017dbe6678ab91
ashwinkk <ashwinkailas@gmail.com>
2017-09-17 12:53:21 +0530

    updated links on page

--- src/partials/logo-bar.js
+++ src/partials/logo-bar.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "../styles/logo-bar.css";
 
 export default function LogoBar(props) {
 	return (
 		<div className="logo-bar-container">
-			<img src="/static/anim-assets/mec_logo.png" alt="mec-logo" />
-			<img src="/static/anim-assets/excel2017.png" alt="excel-logo" />
+			<a href="http://mec.ac.in/">
+				<img src="/static/anim-assets/mec_logo.png" alt="mec-logo" />
+			</a>
+			<Link to="/" className="excel-home">
+				<img src="/static/anim-assets/excel2017.png" alt="excel-logo" />
+			</Link>
 		</div>
 	);
 }
